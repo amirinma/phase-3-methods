4cdede36abc95260732b27bbf8a007363684ae63
Mohammad <zia.ro@live.com>
2022-07-13 00:35:51 -0400

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer!"
+    puts "Hello, #{name}!"
+end
+
+def add arg1, arg2
+    arg1 + arg2
+end
+
+def halve number
+    if number.class != Integer 
+       return nil
+    end
+    number / 2
+end
